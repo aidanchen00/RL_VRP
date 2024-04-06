0275d1870ee09a0d56444151053a659bae3e509f
Aidan Chen <aidanchen00@hotmail.com>
2024-04-06 16:33:06 -0700

    fixed various bugs, including distance calculation, and normalization of distances to depot and truck.

--- src/Julian_Zelda_my_vre.py
+++ src/Julian_Zelda_my_vre.py
@@ -52,10 +52,14 @@ class vehicleRoutingEnv(gym.Env):
         self.action_goback=3
         #Starts move at back to depot
         self.last_move=self.action_goback
+        #Define variable to store num of moves since last selection
+        self.num_last_moves=0
         #Light penalty for swiping left and right
         self.left_right_reward = 0
         #Defining visited and unvisited list
         self.visited=[]
+        #Define max distance
+        self.max_dist=0
         #Length is N customers+1 because includes depot
         self.unvisited=[i for i in range(1,len(self.current_customer_list))]
         
@@ -120,10 +124,14 @@ class vehicleRoutingEnv(gym.Env):
         self.last_move=self.action_goback
         self.unvisited=[i for i in range(1,len(self.current_customer_list))]
         self.visited=[]
+        self.num_last_moves=0
+        self.max_dist=self.find_max_dist()
         state = []
         for sublist in self.current_customer_list:
             if sublist!=self.depot_location:
-                state.extend([sublist[0][0],sublist[0][1],sublist[1],sublist[2],sublist[3],sublist[4][0],sublist[4][1],sublist[5][0],sublist[5][1]])
+                state.extend([sublist[self.index_coord][0],sublist[self.index_coord][1],sublist[self.index_demand],\
+                sublist[self.index_dist_to_depot],sublist[self.index_dist_to_truck],sublist[self.index_pos_rel_depot][0],\
+                sublist[self.index_pos_rel_depot][1],sublist[self.index_pos_rel_truck][0],sublist[self.index_pos_rel_truck][1]])
             else:
                 state.append(self.depot_location[0])
                 state.append(self.depot_location[1])
@@ -131,6 +139,7 @@ class vehicleRoutingEnv(gym.Env):
         #Appending information about currrent and total capacity
         state.append(self.current_truck_capacity)
         state.append(self.total_truck_capacity)
+        
         f_state = self.flat_list(state)
         return f_state, {"cust list":self.current_customer_list, "truck cap":self.current_truck_capacity, "max cap":self.total_truck_capacity, "visited":self.visited, "unvisited":self.unvisited}
 
@@ -144,38 +153,17 @@ class vehicleRoutingEnv(gym.Env):
             #distance to current truck location
             if customer!=self.depot_location: 
                 current_index=self.position_list.index(1)
-                customer[self.index_dist_to_truck]=(m.sqrt((\
-            (customer[self.index_coord][0]-self.depot_location[0])**2)\
-            +(customer[self.index_coord][1]-self.depot_location[1])**2))
-                #position relative to depot calculated before hand (instance creator)
-                #position relative to current location
-                current_index=self.position_list.index(1)
-                #print(f"current_customer_list: {self.current_customer_list}")
-                #print(f"index: {current_index}")
                 #If currently at depot, set position relative to current location to be the same as position relative to depot
                 if current_index==0:
                     customer[self.index_pos_rel_truck]=customer[self.index_pos_rel_depot]
                 else:
                     customer[self.index_pos_rel_truck]=(customer[self.index_coord][0]-self.current_customer_list[current_index][self.index_coord][0],customer[self.index_coord][1]-self.current_customer_list[current_index][self.index_coord][1])
-                #self.reordered_customer_list[current_index]=customer
-        state = []
-        #Create flat state which are the inputs the neural network takes in
-        #The neural network takes flat state as input
-        for sublist in self.current_customer_list:
-            if sublist!=self.depot_location:
-                state.extend([sublist[self.index_coord][0],sublist[self.index_coord][1],sublist[self.index_demand],\
-                sublist[self.index_dist_to_depot],sublist[self.index_dist_to_truck],sublist[self.index_pos_rel_depot][0],\
-                sublist[self.index_pos_rel_depot][1],sublist[self.index_pos_rel_truck][0],sublist[self.index_pos_rel_truck][1]])
-            else:
-                state.append(self.depot_location[0])
-                state.append(self.depot_location[1])
-        state.insert(0,self.position_list)
-        #Appending information about currrent and total capacity
-        state.append(self.current_truck_capacity)
-        state.append(self.total_truck_capacity)
-        f_state = self.flat_list(state)
-        return f_state, {"cust list":self.current_customer_list, "truck cap":self.current_truck_capacity, "max cap":self.total_truck_capacity, "visited":self.visited, "unvisited":self.unvisited}
-        
+                #Calculate distance to truck
+                # KEVIN
+                customer[self.index_dist_to_truck]=m.sqrt((customer[self.index_pos_rel_truck][0])**2+(customer[self.index_pos_rel_truck][1])**2)/self.max_dist
+                customer[self.index_dist_to_depot]=m.sqrt((customer[self.index_pos_rel_depot][0])**2+(customer[self.index_pos_rel_depot][1])**2)/self.max_dist
+
+
     #Checks four options and returns mask
     # num customer plust depot possible actions: 0=Left, 1=right, 2=select, 3=return to depot
     #When mask=1, index is valid. When mask=0, index is invalid at that location
@@ -183,36 +171,31 @@ class vehicleRoutingEnv(gym.Env):
     def get_invalid_ones(self):
         mask=[]
         #If at depot, you cannot "pickup" the depot
-        if self.position_list.index(1)==0:
-            mask.append(self.action_pickup)
-        #If already at depot, then going back to depot is invalid, otherwise depot is always available
+        #If full capacity, don't go back to depot
         if self.current_truck_capacity==self.total_truck_capacity:
             mask.append(self.action_goback)
         #By default depot is only option 
         current_index=self.position_list.index(1)
         left_right_available=False
-        for customer_index in range(len(self.current_customer_list)):
+        numCustAvailable=0
+        for customer_index in range(1,len(self.current_customer_list)):
             #If valid positon at all in any customer, then left and right become available
-            if self.current_customer_list[customer_index][1]<self.current_truck_capacity and customer_index in self.unvisited:
+            if self.current_customer_list[customer_index][self.index_demand]<=self.current_truck_capacity and customer_index in self.unvisited:
                 left_right_available=True
-                break
+                numCustAvailable+=1
         if left_right_available==False:
             mask.append(self.action_left)
             mask.append(self.action_right)
-        #If valid current location pickup is valid
+        else:
+            if numCustAvailable==1:
+                #If valid current location pickup is valid
+                if self.is_valid_position(current_index):
+                    mask.append(self.action_left)
+                    mask.append(self.action_right)
         if self.is_valid_position(current_index)==False:
             mask.append(self.action_pickup)
         #Permanently mask out left
         mask.append(self.action_left)
-        goright=False
-        for customer in self.current_customer_list:
-            if customer[self.index_demand]<=self.current_truck_capacity:
-                goright=True
-            #print(f"increments: {increments}")
-        #print(f"total increments: {increments}")
-        #temp_input = input("Test: ")
-        if goright:
-            mask.append(self.action_right)
         return mask
 
     def get_invalid_ones_w_states(self,state):
@@ -221,38 +204,43 @@ class vehicleRoutingEnv(gym.Env):
         current_capacity=state[len(state)-2]
         total_capacity=state[len(state)-1]
         position_list=state[0:len(self.position_list)]
-        if state[0]==1:
-            mask.append(self.action_pickup)
         #If already at depot, then going back to depot is invalid, otherwise depot is always available
         if current_capacity==total_capacity:
             mask.append(self.action_goback)
         #By default depot is only option 
         current_index=position_list.index(1)
         left_right_available=False
-        demands = state[len(self.position_list)+self.index_demand::len(self.current_customer_list[1])]
+        demands = state[len(self.position_list)+2+2+self.index_demand-1::9]
+        numCustAvailable=0
         for customer_index in range(len(demands)):
             #If valid positon at all in any customer, then left and right become available
             #Checks if demand less than current capacity and demand>0 as a way to see if its been picked up (only works for now where partial fulfillments are not possible)
-            if demands[customer_index]<current_capacity and demands[current_index]>0:
+            if demands[customer_index]<=current_capacity and demands[customer_index]>0:
                 left_right_available=True
-                break
+                numCustAvailable+=1
         if left_right_available==False:
             mask.append(self.action_left)
             mask.append(self.action_right)
+        else:
+            if numCustAvailable==1 and current_index != 0:
+                #If valid current location pickup is valid
+                if demands[current_index-1]<=current_capacity and demands[current_index-1]>0:
+                    mask.append(self.action_left)
+                    mask.append(self.action_right)
         #If valid current location pickup is valid, basically checking is_valid_position conditions in one statement
-        if demands[current_index]>current_capacity or current_index==0:
+        if current_index==0 or demands[current_index-1]>current_capacity:
             mask.append(self.action_pickup)
         #Permanently mask out left
         mask.append(self.action_left)
-        goright = False
-        for element in demands:
-            if element<=current_capacity:
-                goright=True
-        if goright:
-            mask.append(self.action_right)
         return mask
 
-        
+    def find_max_dist(self):
+        max_dist = 0
+        for customer in self.current_customer_list:
+            if customer!=self.depot_location:
+                max_dist+=2*customer[self.index_dist_to_depot]
+        return max_dist
+            
     #determining if location inputted is valid to pick up
     def is_valid_position(self, index):
         valid=False
@@ -278,6 +266,7 @@ class vehicleRoutingEnv(gym.Env):
         #Current customer index in context of original reordered list
         # num customer plust depot possible actions: 0=Left, 1=right, 2=select, 3=return to depot
         current_index=self.position_list.index(1)
+        isDone = False
 #        print(f"Original current index: {current_index}")
         if action==self.action_left:
             #print(f"action: {str(action)}")
@@ -285,30 +274,56 @@ class vehicleRoutingEnv(gym.Env):
             #self.current_customer_list = self.current_customer_list[len(self.current_customer_list)-1] + self.current_customer_list[0:len(self.current_customer_list)-1]
             self.position_list[current_index]=0
             tmp_index=current_index
+            if tmp_index==0:
+                tmp_index=len(self.position_list)-1
+            else:
+                tmp_index-=1
             while self.is_valid_position(tmp_index)==False:
                 if tmp_index==0:
                     tmp_index=len(self.position_list)-1
                 else:
                     tmp_index-=1
             self.position_list[tmp_index]=1
-            self.last_move=self.action_left
-            #Slight negative reward for taking an action without making a move
+            self.num_last_moves+=1
+            #if self.num_last_moves>=len(self.unvisited)+1:
+            #    reward=-self.max_dist
+            #    isDone = True
+            #else:
             reward=self.left_right_reward
+            self.last_move=self.action_left
+           
+            #Slight negative reward for taking an action without making a move
+            reward-=self.left_right_reward
         elif action==self.action_right:
             #print(f"action: {str(action)}")
             #Shifts the list
             self.position_list[current_index]=0
             tmp_index=current_index
+            if tmp_index==len(self.position_list)-1:
+                tmp_index=1
+            else:
+                tmp_index+=1
+            cnt=0
             while self.is_valid_position(tmp_index)==False:
+                cnt+=1
+                if cnt > 1000:
+                    print("ERROR!!!!!")
+                    return
                 if tmp_index==len(self.position_list)-1:
-                    tmp_index=0
+                    tmp_index=1
                 else:
                     tmp_index+=1
             self.position_list[tmp_index]=1
             #self.current_customer_list = self.current_customer_list[1:] + self.current_customer_list[0]
             #Slight negative reward for taking an action without making a move
-            self.last_move=self.action_right
+            self.num_last_moves+=1
+            #if self.num_last_moves>=len(self.unvisited)+1:
+            #    reward=-self.max_dist
+            #    isDone = True
+            #else:
             reward=self.left_right_reward
+            self.last_move=self.action_right
+            
         elif action==self.action_pickup:
             if self.is_valid_position(current_index):
                 #print(f"action: {str(action)}")
@@ -328,27 +343,30 @@ class vehicleRoutingEnv(gym.Env):
                 #Reward is negative distance from current location to old location
                 reward=-self.current_customer_list[current_index][self.index_dist_to_truck]
                 self.last_move=self.action_pickup
+                self.num_last_moves=0
                 self.update()
             else:
                 # action==self.action_pickup and !self.is_valid_position(current_index):
-                reward=-1e16
+                reward=-self.max_dist
         elif action==self.action_goback:
             #Reward is distance to depot
             #print(f"action: {str(action)}")
+            # Reward is the negative distance from where you are to depot
+            reward=-self.current_customer_list[current_index][self.index_dist_to_depot]
             self.position_list[current_index]=0
             self.position_list[0]=1
 #            print(f"Current customer: {self.current_customer_list[current_index]}")
             #Check distance from last visited customer to depot
-            reward=-self.current_customer_list[self.visited[len(self.visited)-1]][self.index_dist_to_depot]
+            # reward=-self.current_customer_list[self.visited[len(self.visited)-1]][self.index_dist_to_depot]
             #Shift the list accordingly
             #self.current_customer_list=self.current_customer_list[self.depot_index:]+self.current_customer_list[0:self.depot_index]
             #Refill truck capacity
             self.last_move=self.action_goback
+            self.num_last_moves=0
             self.current_truck_capacity=self.total_truck_capacity
+            self.update()
         if len(self.unvisited)==0:
             isDone = True
-        else:
-            isDone = False
         #For now truncated=False until conditions are defined
         truncated = False
 #        for i in range(len(self.visited) - 1):
